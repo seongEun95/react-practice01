91eda14ce3708762cec8de1554e2dbfecb1628a6
seong eun <17adm@naver.com>
2023-08-13 11:22:55 +0900

    카운터 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [num, setNum] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="count">
+        <button
+          onClick={() => {
+            setNum(num - 1);
+          }}
         >
-          Learn React @@
-        </a>
-      </header>
+          -
+        </button>
+        <div className="count-num">{num}</div>
+        <button
+          onClick={() => {
+            setNum(num + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 }
